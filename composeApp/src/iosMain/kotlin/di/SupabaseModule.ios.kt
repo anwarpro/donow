a3b882170bc24138d534a616df7286aed51b6f5b
Mohammad Anwar <anwar.hussen.pro@gmail.com>
2024-04-24 13:04:24 +0600

    [fixed] iosBuild issues

--- composeApp/src/iosMain/kotlin/di/SupabaseModule.ios.kt
+++ composeApp/src/iosMain/kotlin/di/SupabaseModule.ios.kt
@@ -3,4 +3,6 @@ package di
 import io.github.jan.supabase.gotrue.AuthConfig
 
 actual fun AuthConfig.platformGoTrueConfig() {
+    scheme = "io.jan.supabase"
+    host = "login"
 }
--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,7 +1,13 @@
 import SwiftUI
+import ComposeApp
 
 @main
 struct iOSApp: App {
+
+   init(){
+      KoinKt.doInitKoin(additionalConfiguration: {_ in})
+   }
+
 	var body: some Scene {
 		WindowGroup {
 			ContentView()
